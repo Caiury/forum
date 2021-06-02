de7b2b3ca8d14f7d4f759a2c176154ebace7252b
Caiury Henrique <caiuryf@gmail.com>
2021-06-02 10:37:28 -0300

    primeiro ENDPOINT

---
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -0,0 +1,26 @@
+package br.com.alura.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+@Controller
+@ResponseBody
+public class TopicoController {
+	
+	@RequestMapping("/topicos")
+	public List<Topico>topicos (){
+		
+		Topico topico = new Topico("Duvida Spring", "Não compila", new Curso("Java","programação"));
+		
+		return Arrays.asList(topico,topico,topico);
+		
+	}
+
+}
