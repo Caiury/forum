a5d7eb91801e623bab3b8a84a05dc302fd7b93fd
Caiury Henrique <caiuryf@gmail.com>
2021-06-10 18:10:57 -0300

    criando metodo para inserir dados

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,15 +1,17 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
@@ -18,7 +20,9 @@ public class TopicoController {
 
 @Autowired	
 private TopicoRepository topicoRepository;
-	
+@Autowired
+private CursoRepository cursoRepository;
+
 	@GetMapping
 	public List<TopicoDto>topicos (String nomeCurso){
 		if(nomeCurso==null) {
@@ -31,7 +35,11 @@ private TopicoRepository topicoRepository;
 		}
 	}
 	
-	
+	@PostMapping
+	public void cadastrar (TopicoForm form) {
+		form.converter(cursoRepository);
+		topicoRepository.save(null);
+	}
 	
 
 }
