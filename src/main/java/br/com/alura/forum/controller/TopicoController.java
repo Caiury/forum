301eb292110017369869f0961ac2f6bd9c5e1ca0
Caiury Henrique <caiuryf@gmail.com>
2021-06-23 14:39:25 -0300

    Usando Pageable para Paginacao

--- src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+
 import java.net.URI;
 import java.util.List;
 
@@ -7,6 +8,9 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -37,18 +42,22 @@ public class TopicoController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDto> topicos(String nomeCurso) {
+	public Page<TopicoDto> topicos(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina, @RequestParam
+			int qtd) {
+		
+		Pageable paginacao = PageRequest.of(pagina, qtd);
 		if (nomeCurso == null) {
-			List<Topico> topicos = topicoRepository.findAll();
+			Page<Topico> topicos = topicoRepository.findAll(paginacao);
 
 			return TopicoDto.converter(topicos);
 		} else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+			Page<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso,paginacao);
 			return TopicoDto.converter(topicos);
 		}
 	}
 
 	@PostMapping
+	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -77,6 +86,7 @@ public class TopicoController {
 	
 	
 	@DeleteMapping("/{id}")
+	@Transactional
 	ResponseEntity<?> deletar (@PathVariable Long id) {
 		
 		topicoRepository.deleteById(id);
--- src/main/java/br/com/alura/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -2,14 +2,15 @@ package br.com.alura.forum.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.alura.forum.modelo.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
 	
-	List<Topico> findByCursoNome(String nomeCurso);
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
 
 }
